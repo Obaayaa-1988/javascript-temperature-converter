a917a4f94c3889cded0ff87d38b6d42093a6b226
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-05-21 20:47:35 +0100

    script added

---
+++ script.js
@@ -0,0 +1,9 @@
+const kelvinValue = document.querySelector(".kelvin > input");
+const celciusValue = document.querySelector(".celcius > input");
+const fahrenheitValue = document.querySelector(".fahrenheit > input");
+
+function convertFromKelvinToCelciusToFahrenheit(){
+    
+
+}
+
