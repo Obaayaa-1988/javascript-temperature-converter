2f34bfd799ca45319b66347a4d529ce21ce72fac
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-05-27 01:22:33 +0100

    added math round

--- script.js
+++ script.js
@@ -2,31 +2,35 @@ const kelvinValue = document.querySelector(".kelvin > input");
 const celciusValue = document.querySelector(".celcius > input");
 const fahrenheitValue = document.querySelector(".fahrenheit > input");
 
+function roundNum(num){
+    return Math.round(num+ 100)
+}
+
 
 function convertFromCelsiusToKelvinToFahrenheit() {
-    const celTempy = celciusValue.value
+    const celTempy = parseFloat(celciusValue.value)
     const kelTempy = celTempy + 273.15
     const fahTempy = (celTempy * (9 / 5)) + 32
-    kelvinValue.value = kelTempy
-    fahrenheitValue.value = fahTempy
+    kelvinValue.value = roundNum(kelTempy)
+    fahrenheitValue.value = roundNum(fahTempy)
 
 }
 
 
 function convertFromKelvinToCelciusToFahrenheit() {
-    const kelTempy = kelvinValue.value
+    const kelTempy = parseFloat(kelvinValue.value)
     const celTempy = kelTempy - 273.15;
     const fahTempy = 9 / 5 * (kelTempy - 273) + 32;
-    celciusValue.value = celTempy;
-    fahrenheitValue.value = fahTempy;
+    celciusValue.value = roundNum(celTempy);
+    fahrenheitValue.value = roundNum(fahTempy);
 }
 
 function convertFromFahrenheitToCelciusToKelvin() {
-    const fahTempy = fahrenheitValue.value
+    const fahTempy = parseFloat(fahrenheitValue.value)
     const celTempy = (fahTempy - 32) * (5/9);
     const kelTempy = (fahTempy + 459.67) * 5/9;
-    celciusValue.value = celTempy;
-    kelvinValue.value = kelTempy
+    celciusValue.value = roundNum(celTempy);
+    kelvinValue.value = roundNum(kelTempy);
 }
 
 
